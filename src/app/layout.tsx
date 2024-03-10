b76fe82891893b8ce39e8e046d203b4aac700975
Chibuzo15 <Anyachris85@gmail.com>
2024-03-10 21:00:31 +0100

    integrate supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +26,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <ToastContainer />
       </body>
     </html>
   );
--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 import { useFormik, Field, FormikProvider } from 'formik';
 import * as Yup from 'yup';
 
+import { toast } from 'react-toastify';
+
+import { supabase } from '@/utils/supabase';
+
 const validationSchema = Yup.object({
   name: Yup.string().required('Name is required'),
   reason: Yup.string().required('Field is required'),
@@ -12,6 +16,8 @@ const validationSchema = Yup.object({
 
 export default function WaitList() {
   // console.log('FormikProvider ', FormikProvider)
+  const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false)
+
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -22,9 +28,103 @@ export default function WaitList() {
     onSubmit: (values) => {
       // Handle form submission here (e.g., send data to server)
       console.log(values);
-      alert('submitted')
+      onSubmitToServer(values)
     },
   });
+
+  const showToast = (message, type) => {
+    if (type == 'success') {
+      console.log('message ', message)
+      toast.success(message || 'Submitted!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+
+    if (type == 'error') {
+      toast.error(message || 'Error encountered!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }
+
+  const onSubmitToServer = async (values) => {
+    // const data = { ...values };
+    setIsSubmitting(true)
+
+    try {
+      // const { data, error } = await supabase
+      //   .from('waitlist') // Replace 'waitlist' with your table name
+      //   .select('*') // Optional: You can select specific columns if needed
+      //   .limit(1); // Limit to 1 row to check existence
+
+
+      // if (error) {
+      //   console.error('Error checking table existence:', error);
+      //   showToast('Error encountered initializing ', 'error')
+      //   // Handle errors (e.g., notify developer or retry)
+      // } else {
+
+      // }
+
+      // Table exists, proceed with data insertion
+      const insertRes = await supabase
+        .from('waitlist')
+        .insert({ // Array of objects
+          name: values.name,
+          email: values.email, // Ensure you're using 'email' for the column
+          reason: values.reason,
+        })
+
+      const { error: insertError } = insertRes
+
+      if (insertError) {
+        console.error('Error inserting data:', insertError);
+
+        showToast(insertError?.details || insertError?.message || 'Error encountered during submission', 'error')
+
+        // Handle insertion errors (e.g., show error message to user)
+      } else {
+        console.log('Data inserted successfully');
+        showToast('Data submitted successfully', 'success')
+
+        // Handle successful insertion (e.g., show success message)
+      }
+
+      setIsSubmitting(false)
+
+    } catch (err) {
+      console.error('Unexpected error:', err);
+      // Handle unexpected errors
+
+      toast.error('Error encountered!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        // transition: Bounce,
+      });
+
+      setIsSubmitting(false)
+    }
+  }
 
   return (
     <main className="flex flex-col items-center pt-[80px] mx-[20px]">
@@ -75,9 +175,10 @@ export default function WaitList() {
             <button
               type="submit"
               onClick={formik.handleSubmit}
+              disabled={isSubmitting ? true : false}
               className="inline-flex text-black items-center bg-[#efefef] rounded-[30px] px-[30px] h-[43px] mt-[32px] mb-[18px]"
             >
-              Join Waitlist
+              {isSubmitting ? 'loading...' : 'Join Waitlist'}
             </button>
           </div>
         </div>
